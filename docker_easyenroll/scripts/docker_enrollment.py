b183bf3292acfb8aba95ac02bd6a0256ec238d00
John Carr <john.carr@unrouted.co.uk>
2018-03-02 12:27:27 +0000

    More refactoring

--- docker_easyenroll/scripts/docker_enrollment.py
+++ docker_easyenroll/scripts/docker_enrollment.py
@@ -1,26 +1,15 @@
 #! /usr/bin/env python3
 
-import os
-
 from docker_easyenroll.store import LocalCertificateStore
 from docker_easyenroll.server import listen_until_enrollment
 from docker_easyenroll.server.validators import StoreCAValidator
+from docker_easyenroll.server.docker import start_dockerd
 
 
 def main():
     store = LocalCertificateStore('/etc/docker/ssl')
-
     listen_until_enrollment(store, StoreCAValidator(store))
-
-    # FIXME: Pass client parameters through to dockerd
-    os.execv('/usr/bin/dockerd', [
-        '/usr/bin/dockerd',
-        '-H', 'fd://',
-        '--tlsverify',
-        '--tlscacert={}'.format(store.get_certificate_path('ca')),
-        '--tlscert={}'.format(store.get_certificate_path('server')),
-        '--tlskey={}'.format(store.get_private_key_path('server')),
-    ])
+    start_dockerd(store)
 
 
 if __name__ == "__main__":
---
+++ docker_easyenroll/server/dockerd.py
@@ -0,0 +1,13 @@
+import os
+
+
+def start_dockerd(store):
+    # FIXME: Pass client parameters through to dockerd
+    os.execv('/usr/bin/dockerd', [
+        '/usr/bin/dockerd',
+        '-H', 'fd://',
+        '--tlsverify',
+        '--tlscacert={}'.format(store.get_certificate_path('ca')),
+        '--tlscert={}'.format(store.get_certificate_path('server')),
+        '--tlskey={}'.format(store.get_private_key_path('server')),
+    ])
