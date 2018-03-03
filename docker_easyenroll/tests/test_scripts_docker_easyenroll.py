16f89c7958f7a1aed5fbd4165029cfbf4d3fe66b
John Carr <john.carr@unrouted.co.uk>
2018-03-03 20:48:35 +0000

    Can use guestinfo CA with standard CLI

--- docker_easyenroll/tests/test_scripts_docker_easyenroll.py
+++ docker_easyenroll/tests/test_scripts_docker_easyenroll.py
@@ -2,6 +2,10 @@ import unittest
 from unittest import mock
 
 from docker_easyenroll.scripts import docker_enrollment
+from docker_easyenroll.server.validators import (
+    GuestInfoCAValidator,
+    StoreCAValidator,
+)
 
 
 class TestEntrypoint(unittest.TestCase):
@@ -9,8 +13,23 @@ class TestEntrypoint(unittest.TestCase):
     @mock.patch('docker_easyenroll.scripts.docker_enrollment.LocalCertificateStore')
     @mock.patch('docker_easyenroll.scripts.docker_enrollment.listen_until_enrollment')
     @mock.patch('docker_easyenroll.scripts.docker_enrollment.start_dockerd')
-    def test_entrypoint(self, a, b, c):
-        docker_enrollment.main()
+    def test_entrypoint(self, a, listen_until_enrollment, c):
+        docker_enrollment.main([])
         assert a.call_count == 1
-        assert b.call_count == 1
+
+        assert listen_until_enrollment.call_count == 1
+        assert isinstance(listen_until_enrollment.call_args[0][1], StoreCAValidator)
+
+        assert c.call_count == 1
+
+    @mock.patch('docker_easyenroll.scripts.docker_enrollment.LocalCertificateStore')
+    @mock.patch('docker_easyenroll.scripts.docker_enrollment.listen_until_enrollment')
+    @mock.patch('docker_easyenroll.scripts.docker_enrollment.start_dockerd')
+    def test_entrypoint_guestinfoca(self, a, listen_until_enrollment, c):
+        docker_enrollment.main(['--guestinfoca', 'ca'])
+        assert a.call_count == 1
+
+        assert listen_until_enrollment.call_count == 1
+        assert isinstance(listen_until_enrollment.call_args[0][1], GuestInfoCAValidator)
+
         assert c.call_count == 1
